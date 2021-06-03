75687304c7a839ac120892ee6b816dc4ea658907
yaya <ymerabety@gmail.com>
2021-06-04 00:41:40 +0200

    adding in to projet

---
+++ public/client.js
@@ -0,0 +1,9 @@
+import SanityClientConstructor from '@sanity/client'
+import sanity from '@sanity/client'
+
+export default SanityClient({
+    projectId: "en682204",
+    dataset: "production"
+
+
+})
